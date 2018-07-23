72f67868176a894544265f73b7add779318cf35d
Thierry1 <floveandroid@sina.com>
2018-07-23 10:34:37 +0800

    在Activity类上和onCreate()上添加了注释

--- app/src/main/java/com/example/t/demo03/MainActivity.java
+++ app/src/main/java/com/example/t/demo03/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.t.demo03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//这是添加的注释
 public class MainActivity extends AppCompatActivity {
 
+    //一般在此方法初始化控件
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
